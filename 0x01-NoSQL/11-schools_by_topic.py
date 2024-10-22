d8a414ae213c58db67c67ba9b2da530a79616020
Bonolo <easytmashabela@gmail.com>
2024-10-22 15:06:33 -0700

    Where can I learn Python?

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -7,6 +7,6 @@ import pymongo
 
 def schools_by_topic(mongo_collection, topic):
     """
-    find by topic
+    Find by topic
     """
-    return mongo_collection.find({"topic": topic})
+    return mongo_collection.find({"topics": topic})
